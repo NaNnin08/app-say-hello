151e9a6a686b71d3adf688c1b61c6a4a4f66ad58
nida <nida@code.id>
2024-12-05 21:11:23 +0700

    feat: app go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/NaNnin08/app-say-hello
+
+go 1.22.4
+
+require github.com/NaNnin08/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/NaNnin08/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello("Nida"))
+}
